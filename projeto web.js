3b1f69758766a48ecb7a2297fd45f846b59588f6
Alma-x13 <nathancarriel92@gmail.com>
2024-12-05 09:40:39 -0300

    coisas novas

--- projeto web.js
+++ projeto web.js
@@ -1,3 +1,71 @@
+function name(params) {
+
+let conttermometro=0;
+let contradio=0;
+let contcaderno=0;
+let contemf=0;
+let contultravioleta=0;
+let contorbe=0;
+let contdots=0;
+
+let radios = document.querySelectorAll('input[type="radio"]') 
+console.log(radios)
+    
+}
+
+function name(params) {
+
+    let contBanshee=0;
+    let contDemon=0;
+    let contDeogen=0;
+    let contGoryo=0;
+    let contHantu=0;
+    let contJinn=0;
+    let contMare=0;
+    let contMoroi=0;
+    let contMyling=0;
+    let contObake=0;
+    let contOni=0;
+    let contOnryo=0;
+    let contPhantom=0;
+    let contPoltergeist=0;
+    let contRaiju=0;
+    let contRevenant=0;
+    let contShade=0;
+    let contSpirit=0;
+    let contThaye=0;
+    let contMimic=0;
+    let contThe_Twins=0;
+    let contWraith=0;
+    let contYokai=0;
+    let contYurei=0;
+    
+    let radios = document.querySelectorAll('input[type="radio"]') 
+    console.log(radios)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
