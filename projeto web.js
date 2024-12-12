f295e38d78e24cf7fb8148f990b1bf3f7a1d6d3c
Almax2013@ <nathancarriel92@gmail.com>
2024-12-12 10:09:28 -0300

    arrumando problema da perguntas

--- projeto web.js
+++ projeto web.js
@@ -1,3 +1,112 @@
+function testar() {
+    // Contagem de votos
+    let contnaruto = 0;
+    let contgoku = 0;
+    let contluffy = 0;
+    let contichigo = 0;
+    let contyugi = 0;
+    let contash = 0;
+    let contAinz_Ooal_Gow = 0;
+
+    // Array com todas as perguntas
+    let perguntas = [
+        'pergunta1', 'pergunta2', 'pergunta3', 'pergunta4', 'pergunta5',
+        'pergunta6', 'pergunta7', 'pergunta8', 'pergunta9', 'pergunta10'
+    ];
+
+    perguntas.forEach(pergunta => {
+        let radios = document.querySelectorAll(`input[name="${pergunta}"]:checked`);
+        console.log(radios)
+        radios.forEach(e => {
+            switch (e.value) {
+
+                case "Ainz_Ooal_Gow":
+                    contAinz_Ooal_Gow++;
+                    break;
+                case "Naruto":
+                    contnaruto++;
+                    break;
+                case "Goku":
+                    contgoku++;
+                    break;
+                case "Luffy":
+                    contluffy++;
+                    break;
+                case "Ichigo":
+                    contichigo++;
+                    break;
+                case "Yugi":
+                    contyugi++;
+                    break;
+                case "Ash":
+                    contash++;
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
+
+
+    let resultado = `
+        naruto = ${contnaruto}<br> 
+        goku = ${contgoku}<br> 
+        luffy = ${contluffy}<br> 
+        ichigo = ${contichigo}<br>
+        yugi = ${contyugi}<br>
+        ash = ${contash}<br>
+        Ainz Ooal Gow = ${contAinz_Ooal_Gow}
+    `;
+    document.getElementById("resultado").innerHTML = resultado;
+
+    localStorage.setItem('naruto', contnaruto);
+    localStorage.setItem('goku', contgoku);
+    localStorage.setItem('luffy', contluffy);
+    localStorage.setItem('ichigo', contichigo);
+    localStorage.setItem('yugi', contyugi);
+    localStorage.setItem('ash', contash);
+    localStorage.setItem('Ainz_Ooal_Gow', contAinz_Ooal_Gow);
+
+    // Verificando qual personagem tem mais votos
+    let maior = 0;
+    let pagina = '';
+
+    if (contnaruto > maior) {
+        maior = contnaruto;
+        pagina = 'naruto';
+    }
+    if (contgoku > maior) {
+        maior = contgoku;
+        pagina = 'goku';
+    }
+    if (contluffy > maior) {
+        maior = contluffy;
+        pagina = 'luffy';
+    }
+    if (contichigo > maior) {
+        maior = contichigo;
+        pagina = 'ichigo';
+    }
+    if (contyugi > maior) {
+        maior = contyugi;
+        pagina = 'yugi';
+    }
+    if (contash > maior) {
+        maior = contash;
+        pagina = 'ash';
+    }
+    if (contAinz_Ooal_Gow > maior) {
+        maior = contAinz_Ooal_Gow;
+        pagina = 'Ainz_Ooal_Gow';
+    }
+
+    // Redirecionar para a página do personagem vencedor
+    setTimeout(() => { 
+        window.location.href = pagina + '.html'; 
+    }, 3000);
+}
+
+
 
     
 
